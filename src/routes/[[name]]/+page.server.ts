4706c1581167dbdfeaa49654d1ef868a497189e2
Johnny Magrippis <j@magrippis.com>
2023-04-15 14:33:22 +0200

    ♻️  cache on the load method instead

--- src/routes/[[name]]/+page.server.ts
+++ src/routes/[[name]]/+page.server.ts
@@ -1,10 +1,15 @@
 import {createChatCompletion} from '$lib/server/openai'
 import type {PageServerLoad} from './$types'
 
-export const load = (async ({params}) => {
+const FIVE_MINUTES_IN_SECONDS = 60 * 5
+
+export const load = (async ({params, setHeaders}) => {
 	const {name = 'World'} = params
 
 	const greeting = await createChatCompletion(name)
 
+	setHeaders({
+		'cache-control': `max-age=${FIVE_MINUTES_IN_SECONDS}`,
+	})
 	return {greeting}
 }) satisfies PageServerLoad
