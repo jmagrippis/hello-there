00854d5dc1c39e129eca22f10fef19deb3b3985a
Johnny Magrippis <j@magrippis.com>
2023-04-15 18:58:34 +0200

    ♻️  use @upstash/redis

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,10 +1,9 @@
-import {Redis} from 'ioredis'
-import {REDIS_PASSWORD, REDIS_PORT, REDIS_URL} from '$env/static/private'
+import {Redis} from '@upstash/redis'
+import {REDIS_URL, REDIS_TOKEN} from '$env/static/private'
 
 const redis = new Redis({
-	host: REDIS_URL,
-	password: REDIS_PASSWORD,
-	port: parseInt(REDIS_PORT),
+	url: REDIS_URL,
+	token: REDIS_TOKEN,
 })
 
 export const getGreeting = (name: string) => redis.get(name)
