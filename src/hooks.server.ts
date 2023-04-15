280294ba1906e1362927d30aa51ae18256893224
Johnny Magrippis <j@magrippis.com>
2023-04-15 14:28:19 +0200

    ⚡️  cache responses

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,17 @@ import type {Handle} from '@sveltejs/kit'
 
 import {isValidTheme} from '$lib/stores/theme'
 
+const FIVE_MINUTES_IN_SECONDS = 60 * 5
+
 export const handle: Handle = async ({event, resolve}) => {
 	const theme = event.cookies.get('theme') ?? 'auto'
 	if (isValidTheme(theme)) {
 		event.locals.theme = theme
 	}
+
+	event.setHeaders({
+		'Cache-Control': `max-age=${FIVE_MINUTES_IN_SECONDS}`,
+	})
 
 	return resolve(event, {
 		transformPageChunk: ({html}) => html.replace('%THEME%', theme),
