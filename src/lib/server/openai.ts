783b0f42cee57505fe6ecd234d4d4a595ef77559
Johnny Magrippis <j@magrippis.com>
2023-04-15 11:51:12 +0200

    ✨  ask openai for a greeting!

---
+++ src/lib/server/openai.ts
@@ -0,0 +1,57 @@
+import {OPENAI_API_KEY} from '$env/static/private'
+
+type ChatCompletion = {
+	id: string
+	object: string
+	created: number
+	choices: [
+		{
+			index: number
+			message: {
+				role: string
+				content: string
+			}
+			finish_reason: string
+		}
+	]
+	usage: {
+		prompt_tokens: number
+		completion_tokens: number
+		total_tokens: number
+	}
+}
+
+const isChatCompletion = (data: unknown): data is ChatCompletion =>
+	typeof data === 'object' &&
+	!!(data as ChatCompletion).choices?.[0].message?.content
+
+export const createChatCompletion = async (name: string) => {
+	const response = await fetch('https://api.openai.com/v1/chat/completions', {
+		method: 'POST',
+		headers: new Headers({
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${OPENAI_API_KEY}`,
+		}),
+		body: JSON.stringify({
+			model: 'gpt-4',
+			messages: [
+				{
+					role: 'user',
+					content: `Please respond as if you just saw ${name} dropping by, and you wanted to greet them. Make sure to include a pun!`,
+				},
+			],
+		}),
+	})
+
+	if (!response.ok) {
+		throw new Error(`Could not ask OpenAI to greet ${name}!`)
+	}
+
+	const json = await response.json()
+
+	if (!isChatCompletion(json)) {
+		throw new Error(`Unexpected response from OpenAI when greeting ${name}!`)
+	}
+
+	return json.choices[0].message.content
+}
--- src/routes/[[name]]/+page.server.ts
+++ src/routes/[[name]]/+page.server.ts
@@ -1,9 +1,10 @@
+import {createChatCompletion} from '$lib/server/openai'
 import type {PageServerLoad} from './$types'
 
-export const load = (({params}) => {
+export const load = (async ({params}) => {
 	const {name = 'World'} = params
 
-	const greeting = `Hello there ${name}, looking good!`
+	const greeting = await createChatCompletion(name)
 
 	return {greeting}
 }) satisfies PageServerLoad
